f1454ca30528ef3ed00a827f52a4e14715f2d2e0
unknown <anishkumar92@gmail.com>
2023-09-25 14:23:26 +0530

    added image and contact section

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -3,8 +3,17 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
-  styleUrls: ['./contact.component.scss']
+  styleUrls: ['./contact.component.scss'],
 })
 export class ContactComponent {
-
+  formData = {
+    name: '',
+    email: '',
+    message: '',
+  };
+  onSubmit() {
+    // Handle form submission here
+    console.log('Form submitted with data:', this.formData);
+    // You can add code here to send the form data to a server or perform other actions.
+  }
 }
